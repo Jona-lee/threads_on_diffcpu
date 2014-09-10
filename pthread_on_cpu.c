b99260c5514a6391b84995f793dc63fc35d5acb9
Jona-lee <javenly@gmail.com>
2014-09-10 10:53:43 +0800

    Update pthread_on_cpu.c

--- pthread_on_cpu.c
+++ pthread_on_cpu.c
@@ -1,3 +1,14 @@
+/*
+ *
+ *  Copyright (c) 2013 javenly@gmail.com
+ * 
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ */
+
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <pthread.h>
